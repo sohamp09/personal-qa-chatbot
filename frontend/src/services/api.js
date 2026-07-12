c89ae0129e5e13fad6551f4db18c5d1af393872e
soham patel <sohamp2911@gmail.com>
2026-07-12 13:07:41 +0530

    Add URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+    baseURL: "http://127.0.0.1:8000" ,
+    baseURL: "https://personal-qa-chatbot.onrender.com/",
 });
 
 export default api;
